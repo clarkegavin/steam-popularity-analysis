508e90db956c2b35d9e57e084b5beb10f320428a
Gavin Clarke <clarke.gavin@gmail.com>
2025-11-16 14:48:27 +0000

    added xgboost and baseline experiments for tf-idf, word2vec and bert sentence transfomers.

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,16 @@ from .factory import ModelFactory
 from . import naive_bayes_model
 from .knn_model import KNNClassificationModel
 from .naive_bayes_model import NaiveBayesClassificationModel
+from .xgboost_model import XGBoostClassificationModel
 
 # Register models
 ModelFactory.register_model("naive_bayes", NaiveBayesClassificationModel)
 ModelFactory.register_model('knn', KNNClassificationModel)
+ModelFactory.register_model("xgboost", XGBoostClassificationModel)
 
 __all__ = [
     "ModelFactory",
     "naive_bayes_model",
-  'knn_model',]
+  "knn_model",
+ "xgboost_model",
+]
